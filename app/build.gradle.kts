8a0cc190e42616f70f4bb28328715dfd0320a4a6
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2023-11-07 10:29:30 +0300

    feature: images glide loader implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
